0150fc39b34de882c8a605f243a79337b59ee6b8
Benjamin Hättasch <Benjamin.Haettasch@igd.fraunhofer.de>
2014-05-26 14:57:27 +0200

    Added basic templates and URL configuration

--- src/OpenRadioTool/service/views.py
+++ src/OpenRadioTool/service/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    """
+    Index
+    :param request: HTTP Request handled
+    """
+    context = {'title': 'Service Display'}
+    return render(request, 'service/index.html', context)
